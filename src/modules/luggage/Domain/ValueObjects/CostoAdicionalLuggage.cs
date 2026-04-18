4adeae96953794da6406cbc5e56d28927b446e17
PaulaV09 <pviviescas@unab.edu.co>
2026-04-18 17:06:32 -0500

    feat: :sparkles: valueobjects bloque equipaje

---
+++ src/modules/luggage/Domain/ValueObjects/CostoAdicionalLuggage.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirTicketSystem.src.modules.luggage.Domain.ValueObjects
+{
+    public class CostoAdicionalLuggage
+    {
+        
+    }
+}
