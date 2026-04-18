95de694f0e63282bcf3c6eb89cddced4e294e323
Ksanti-monsalve <dianamon1919@gmail.com>
2026-04-18 16:48:35 -0500

    feat: :sparkles: valueobjects bloque tarifas y equipaje

---
+++ src/modules/fare/Domain/ValueObjects/ActivaFare.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirTicketSystem.src.modules.fare.Domain.ValueObjects
+{
+    public class ActivaFare
+    {
+        
+    }
+}
