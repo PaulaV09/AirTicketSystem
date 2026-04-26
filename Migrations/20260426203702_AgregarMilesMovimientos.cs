a368ec3f59cd2bac991383640397458c7fe3dc48
PaulaV09 <pviviescas@unab.edu.co>
2026-04-26 15:42:06 -0500

    feat: :sparkles: Migracion de nueva tabla millas movimientos

---
+++ Migrations/20260426203702_AgregarMilesMovimientos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirTicketSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarMilesMovimientos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "miles_movimientos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    cuenta_id = table.Column<int>(type: "int", nullable: false),
+                    reserva_id = table.Column<int>(type: "int", nullable: true),
+                    tipo = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    millas = table.Column<int>(type: "int", nullable: false),
+                    fecha = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    descripcion = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_miles_movimientos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_miles_movimientos_cuentas_millas_cuenta_id",
+                        column: x => x.cuenta_id,
+                        principalTable: "cuentas_millas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_miles_movimientos_reservas_reserva_id",
+                        column: x => x.reserva_id,
+                        principalTable: "reservas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_miles_movimientos_cuenta_id",
+                table: "miles_movimientos",
+                column: "cuenta_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_miles_movimientos_reserva_id",
+                table: "miles_movimientos",
+                column: "reserva_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "miles_movimientos");
+        }
+    }
+}
