b97b65981024639689e31a25619356957fbc694d
PaulaV09 <pviviescas@unab.edu.co>
2026-04-26 15:51:21 -0500

    feat: :sparkles: Migracion de modificacion de la tabla payment

---
+++ Migrations/20260426204837_AgregarMilesEnPago.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirTicketSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarMilesEnPago : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "miles_usadas",
+                table: "pagos",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "miles_usadas",
+                table: "pagos");
+        }
+    }
+}
